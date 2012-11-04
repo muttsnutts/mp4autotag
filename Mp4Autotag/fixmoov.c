b5ef643f8cfbd27cf190a2cf19d7b474506967af
popmedic <popmedic@gmail.com>
2012-11-03 18:52:20 -0600

    Fixed optimize for network rename problem.

--- Mp4Autotag/fixmoov.c
+++ Mp4Autotag/fixmoov.c
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <inttypes.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
 #ifdef __MINGW32__
 #define fseeko(x,y,z)  fseeko64(x,y,z)
@@ -56,6 +59,65 @@
 
 #define ATOM_PREAMBLE_SIZE 8
 #define COPY_BUFFER_SIZE 1024
+
+int cp(const char *to, const char *from)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+	
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+	
+    fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    if (fd_to < 0)
+        goto out_error;
+	
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+		
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+			
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+	
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+		
+        /* Success! */
+        return 0;
+    }
+	
+out_error:
+    saved_errno = errno;
+	
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+	
+    errno = saved_errno;
+    return -1;
+}
 
 int fixMOOVFILE(char* source, char* dest)
 {
@@ -310,7 +372,8 @@ int fixMOOV(char* file)
 	{
 		if(rename(tmpFilename, file) != 0) 
 		{
-			perror("Unable to use rename.");
+			perror("Unable to use rename. Using cp.\n");
+			cp(file, tmpFilename);
 		}
 	}
 	unlink(tmpFilename);
